58a27d97bad067dc55b2629fb28a748a06f5f5f4
Rushil Ekhande <rushilekhande@gmail.com>
2025-04-24 19:27:53 +0530

    fd

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const chalk = require('chalk');
-const boxen = require('boxen');
+// index.js
 
 process.stdin.setEncoding('utf8');
 
 let data = '';
 
+// Listen for data input
 process.stdin.on('data', chunk => {
     data += chunk;
 });
 
-process.stdin.on('end', () => {
-    const output = data
-        .replace(/\\n/g, '\n')
-        .replace(/\\t/g, '\t')
-        .replace(/\\"/g, '"')
-        .replace(/^"|"$/g, '');
+// Listen for end of input
+process.stdin.on('end', async () => {
+    // Dynamically import the packages (for ESM compatibility)
+    const chalk = (await import('chalk')).default;
+    const boxen = (await import('boxen')).default;
 
+    // Clean up \n and formatting
+    const output = data
+        .replace(/\\n/g, '\n')  // unescape \n
+        .replace(/\\t/g, '\t')  // unescape \t
+        .replace(/\\"/g, '"')   // unescape quotes
+        .replace(/^"|"$/g, ''); // remove wrapping quotes if any
+
+    // Try to parse and pretty-print JSON
     let prettyOutput = output;
     try {
         const parsed = JSON.parse(output);
         prettyOutput = JSON.stringify(parsed, null, 2);
     } catch { }
 
+    // Output the formatted result in a box
     console.log(
         boxen(chalk.green(prettyOutput), {
             padding: 1,
